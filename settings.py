ba4e3ac97a33d61890712e4d8b6a5fa9b2682cb0
Kirienko-Alyona <ladybird.miami@gmail.com>
2025-09-19 16:52:13 +0300

    change colors

--- settings.py
+++ settings.py
@@ -6,10 +6,10 @@ import pygame
 WIDTH, HEIGHT = 500, 500
 
 # Кольори
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-PURPLE = (128, 0, 128)
-GREEN = (0, 255, 0)
+BLUE = (65, 105, 225)       # Royal Blue
+YELLOW = (255, 204, 102)    # Mellow Yellow
+PURPLE = (153, 50, 204)     # Dark Orchid
+GREEN = (60, 179, 113)      # Medium Sea Green
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (128, 128, 128)
@@ -22,11 +22,16 @@ SQUARE_SPEED = 10
 # Налаштування кіл
 CIRCLE_RADIUS = 20
 CIRCLE_FALL_SPEED = 5
-CIRCLE_SPAWN_TIME_RANGE = (1000, 3000)  # мс
+CIRCLE_SPAWN_TIME_RANGE = (100, 3000)  # мс
 
 # Налаштування гри
 FPS = 30
 COLOR_CHANGE_TIME_RANGE = (5000, 10000)  # мс
 
-# Шрифти
+# Шрифти перенесені в файл menu.py
 # FONT = pygame.font.SysFont(None, 30)
+
+FIGURES = {
+    # CIRCLE: {},
+
+}
