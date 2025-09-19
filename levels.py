5a3c65a36231ee045ebc76add8db131e1040760b
Kirienko-Alyona <ladybird.miami@gmail.com>
2025-09-19 18:26:16 +0300

    added levels

--- levels.py
+++ levels.py
@@ -9,17 +9,61 @@ LEVEL_CONFIGS = {
         "change_square_color": False,
         "win_score": 3,
         "circle_speed": 5,
+        "circle_color": GREEN,
+        # "figure_type": CIRCLE,
     },
     2: {
         "square_color": "random",
         "change_square_color": True,
         "win_score": 3,
-        "circle_speed": 7,
+        "circle_speed": 6,
     },
     3: {
         "square_color": "random",
         "change_square_color": True,
         "win_score": 3,
+        "circle_speed": 7,
+    },
+    4: {
+        "square_color": "random",
+        "change_square_color": True,
+        "win_score": 3,
+        "circle_speed": 8,
+    },
+    5: {
+        "square_color": "random",
+        "change_square_color": True,
+        "win_score": 3,
         "circle_speed": 9,
-    }
+    },
+    6: {
+        "square_color": "random",
+        "change_square_color": True,
+        "win_score": 3,
+        "circle_speed": 10,
+    },
+    7: {
+        "square_color": "random",
+        "change_square_color": True,
+        "win_score": 3,
+        "circle_speed": 11,
+    },
+    8: {
+        "square_color": "random",
+        "change_square_color": True,
+        "win_score": 3,
+        "circle_speed": 12,
+    },
+    9: {
+        "square_color": "random",
+        "change_square_color": True,
+        "win_score": 3,
+        "circle_speed": 13,
+    },
+    10: {
+        "square_color": "random",
+        "change_square_color": True,
+        "win_score": 3,
+        "circle_speed": 14,
+    },
 }
